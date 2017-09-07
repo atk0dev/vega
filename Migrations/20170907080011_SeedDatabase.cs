cc7143e20e9ce7002f1aedfc1fc0441dc99ac58f
Andrii <at@winkas.dk>
2017-09-07 11:38:54 +0300

    Adding API endpoint

---
+++ Migrations/20170907080011_SeedDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Makes (Name) values ('Make 1')");
+            migrationBuilder.Sql("insert into Makes (Name) values ('Make 2')");
+            migrationBuilder.Sql("insert into Makes (Name) values ('Make 3')");
+
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 1 Model A', (select Id from Makes where Name='Make 1'))");
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 1 Model B', (select Id from Makes where Name='Make 1'))");
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 1 Model C', (select Id from Makes where Name='Make 1'))");
+
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 2 Model A', (select Id from Makes where Name='Make 2'))");
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 2 Model B', (select Id from Makes where Name='Make 2'))");
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 2 Model C', (select Id from Makes where Name='Make 2'))");
+
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 3 Model A', (select Id from Makes where Name='Make 3'))");
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 3 Model B', (select Id from Makes where Name='Make 3'))");
+            migrationBuilder.Sql("insert into Models (Name, MakeId) values ('Make 3 Model C', (select Id from Makes where Name='Make 3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Models");
+            migrationBuilder.Sql("delete from Makes where name in ('Make 1', 'Make 2', 'Make 3')");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using vega.Persistence;
 
 namespace vega
 {
@@ -23,6 +26,11 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+
+            //string connStr = Configuration["ConnectionStrings:Default"];
+            string connStr = Configuration.GetConnectionString("Default");
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(connStr));
             services.AddMvc();
         }
 
